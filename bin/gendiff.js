d012f18b4c243757266ea0c5983da33d870a8294
KirSivogr <kirillsivograkovv@mail.ru>
2022-11-13 00:41:23 +0300

    add gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+const program = new Command();
+
+program
+    .description('Compares two configuration files and shows a difference.')
+    .version('2.2.8')
+    .option('-f, --format <type>', 'output format')
+    .argument('<filepath1> <filepath2>')
+
+
+program.parse();
