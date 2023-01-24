207ea037bb7c1564a0571fc8cc5dbad61aa1e6fb
KirSivogr <kirillsivograkovv@mail.ru>
2023-01-25 00:27:16 +0300

    fixed parser

--- src/parser.js
+++ src/parser.js
@@ -13,18 +13,18 @@ export default function genDiff(firstFile, secondFile) {
     let resultDiff = mergeArr.reduce((acum, property) => {
         if (secondDataObj[property[0]] && firstDataObj[property[0]]) {
             if (secondDataObj[property[0]] == firstDataObj[property[0]]) {
-                acum += `   ${property[0]}: ${property[1]}\n`;
+                acum += `    ${property[0]}: ${property[1]}\n`;
             }
             else {
-                acum += ` - ${property[0]}: ${firstDataObj[property[0]]}\n`;
-                acum += ` + ${property[0]}: ${secondDataObj[property[0]]}\n`;
+                acum += `  - ${property[0]}: ${firstDataObj[property[0]]}\n`;
+                acum += `  + ${property[0]}: ${secondDataObj[property[0]]}\n`;
             }
         }
         else if (secondDataObj[property[0]]) {
-            acum += ` + ${property[0]}: ${secondDataObj[property[0]]}\n`;
+            acum += `  + ${property[0]}: ${secondDataObj[property[0]]}\n`;
         }
         else {
-            acum += ` - ${property[0]}: ${firstDataObj[property[0]]}\n`;
+            acum += `  - ${property[0]}: ${firstDataObj[property[0]]}\n`;
         }
         return acum;
     }, '{\n');
