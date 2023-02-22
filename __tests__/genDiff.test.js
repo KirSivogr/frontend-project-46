d4dd889bbebb29e09168e922521a12e0529189f3
KirSivogr <kirillsivograkovv@mail.ru>
2023-02-22 11:51:24 +0300

    step 5-8

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,9 +1,25 @@
-import genDiff from "../src/parser.js";
+import genDiff from "../src/formatters/index.js";
 import { expect, test } from '@jest/globals';
 import fs from "fs";
 import path from "path";
 
-test('gendiff', () => {
-    const res = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', 'res.txt'), "utf-8");
+test('stylish', () => {
+    const res = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', 'resStylish.txt'), "utf-8");
     expect(genDiff('file1.json', 'file2.json')).toEqual(res);
+    expect(genDiff('file3.yml', 'file4.yml')).toEqual(res);
 });
+
+test('plain', () => {
+    const res = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', 'resPlain.txt'), "utf-8");
+    expect(genDiff('file1.json', 'file2.json', 'plain')).toEqual(res);
+    expect(genDiff('file3.yml', 'file4.yml', 'plain')).toEqual(res);
+});
+
+test('json', () => {
+    const res = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', 'resJson.txt'), "utf-8");
+    expect(genDiff('file1.json', 'file2.json', 'json')).toEqual(res);
+    expect(genDiff('file3.yml', 'file4.yml', 'json')).toEqual(res);
+});
+
+
+
---
+++ src/formatters/index.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import path from 'path';
+import makeStylish from "./stylish.js";
+import makePlain from "./plain.js";
+import makeJson from "./json.js"
+import parse from '../parsers.js'
+
+export default function genDiff(firstFile, secondFile, format = 'stylish') {
+    const firstJSONFileData = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', firstFile), "utf-8");
+    const secondJSONFileData = fs.readFileSync(path.resolve(process.cwd(), '__fixtures__', secondFile), "utf-8");
+    const formatParse1 = parse(firstFile);
+    const formatParse2 = parse(secondFile);
+    const firstDataObj = formatParse1(firstJSONFileData);
+    const secondDataObj = formatParse2(secondJSONFileData);
+
+    if (format === 'stylish') {
+        return makeStylish(firstDataObj, secondDataObj);
+    }
+    else if (format === 'plain') {
+        return makePlain(firstDataObj, secondDataObj);
+    }
+    else {
+        return makeJson(firstDataObj, secondDataObj);
+    }
+}
