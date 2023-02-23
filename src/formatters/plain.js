37c1772041600cffb3ca1768538d09508dd939a5
Kirill Sivograkov <110250397+KirSivogr@users.noreply.github.com>
2023-02-23 19:03:46 +0300

    fix plain

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,56 +1,36 @@
-import _ from "lodash";
-import compare from "./compare.js";
+import _ from 'lodash';
 
-const inType = (value) => {
-    if (typeof value === 'string') {
-        return `'${value}'`;
-    } if (_.isObject(value)) {
-        return '[complex value]';
-    } return value;
+const stringify = (value) => {
+  if (_.isObject(value)) {
+    return '[complex value]';
+  } if (typeof value === 'string') {
+    return `'${value}'`;
+  }
+  return String(value);
 };
 
-const stringify = (obj, string) => {
-    const iter = (obj1, string1) => {
-        if (!Array.isArray(obj1)) {
-            const {sign, key, value} = obj1;
-            if (sign === '+') {
-                return `.${key}' was added with value: ${inType(value)}`;
-            }
-            else if (sign === '-') {
-                return `.${key}' was removed`;
-            }
-            else if (sign === 'not same') {
-                return `.${key}' was updated. From ${inType(value[0])} to ${inType(value[1])}`;
-            }
-            else if (sign === 'o') {
-                return `.${key}${iter(value, string1 + '.' + key)}`;
-            }
-        }
-        return `${obj1.map((item) => { if (item['sign'] !== 'same') return `${iter(item, string1)}`})
-            .filter(Boolean).
-            join(`\nProperty '${string1}`)}`;
-    }
-    return iter(obj, string);
-}
+export default (data) => {
+  const iter = (node, key = '') => {
+    const result = node.flatMap((item) => {
+      const newKeys = [...key, item.key];
 
-export default function makePlain (obj1, obj2) {
-    const resultOfCompare = compare(obj1, obj2);
-    const iter = (item) => {
-        return item.map((obj) => {
-            const {sign, key, value} = obj;
-            if (sign === 'o') {
-                return `Property '${key}${stringify(value, key)}`;
-            }
-            if (sign === '+') {
-                return `Property '${key}' was added with value: ${inType(value)}`;
-            }
-            if (sign === '-') {
-                return `Property '${key}' was removed`;
-            }
-            if (sign === 'not same') {
-                return `Property '${key}' was updated. From '${inType(value[0])}' to '${inType(value[1])}'`;
-            }
-        }).join('\n');
-    }
-    return iter(resultOfCompare, '');
-}
+      switch (item.type) {
+        case 'object':
+          return iter(item.children, newKeys);
+        case 'added':
+          return `Property '${newKeys.join('.')}' was added with value: ${stringify(item.val)}`;
+        case 'deleted':
+          return `Property '${newKeys.join('.')}' was removed`;
+        case 'unchanged':
+          return null;
+        case 'changed':
+          return `Property '${newKeys.join('.')}' was updated. From ${stringify(item.val1)} to ${stringify(item.val2)}`;
+        default:
+          return `Unknown type ${item.type}`;
+      }
+    });
+
+    return result.filter((item) => item !== null).join('\n');
+  };
+  return iter(data, []);
+};
