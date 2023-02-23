2b02981dca8f7bc83d86cf90a0701c8a19338c57
sivog <sivograkovkirill@mail.ru>
2023-02-23 20:52:19 +0300

    final fix

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -39,20 +39,20 @@ const stringify = (obj, string) => {
 export default function makePlain(obj1, obj2) {
   const resultOfCompare = compare(obj1, obj2);
   const iter = (item) => item.map((obj) => {
-      const { sign, key, value } = obj;
-      if (sign === 'o') {
-        return `Property '${key}${stringify(value, key)}`;
-      }
-      if (sign === '+') {
-        return `Property '${key}' was added with value: ${inType(value)}`;
-      }
-      if (sign === '-') {
-        return `Property '${key}' was removed`;
-      }
-      if (sign === 'not same') {
+    const { sign, key, value } = obj;
+    if (sign === 'o') {
+      return `Property '${key}${stringify(value, key)}`;
+    }
+    if (sign === '+') {
+      return `Property '${key}' was added with value: ${inType(value)}`;
+    }
+    if (sign === '-') {
+      return `Property '${key}' was removed`;
+    }
+    if (sign === 'not same') {
       return `Property '${key}' was updated. From '${inType(value[0])}' to '${inType(value[1])}'`;
-      }
-      return '';
+    }
+    return '';
   }).filter(Boolean).join('\n');
   return iter(resultOfCompare, '');
 }
